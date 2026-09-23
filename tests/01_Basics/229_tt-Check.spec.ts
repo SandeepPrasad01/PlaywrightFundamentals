4d3dc25fcf909027f4e9a883696dbc50c0747e0f
SandeepPrasad01 <SandeepPrasad01@users.noreply.github.com>
2026-09-24 03:11:45 +0530

    Update project files

---
+++ tests/01_Basics/229_tt-Check.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://app.thetestingacademy.com/playwright/multiple_element_filter');
+  await page.getByRole('textbox', { name: 'Email Address' }).click();
+  await page.getByRole('textbox', { name: 'Email Address' }).fill('Sandeep');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('123456');
+  await page.getByTestId('login-button').click();
+  await page.getByRole('checkbox', { name: 'Remember me' }).check();
+  await page.getByTestId('login-button').click();
+  await page.getByTestId('form-forgot-password').click();
+  //await page.waitForTimeout(5000); for holding the page for 5 seconds
+});
